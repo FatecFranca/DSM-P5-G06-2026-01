510ef62e06f769943054054158652db49b8140fd
gpessoni <pessonigs@gmail.com>
2026-04-23 11:48:12 -0300

    Fix: Add the .env in project

--- web/lib/api.ts
+++ web/lib/api.ts
@@ -1,6 +1,7 @@
+// env
+    
 // ─── Base URL ─────────────────────────────────────────────────────────────────
-const BASE_URL = 'http://localhost:3000/api';
-
+const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 // ─── Types ────────────────────────────────────────────────────────────────────
 
 export interface ApiUsuario {
